377c1a49ff96a8809dbe5afee5e1e78c1a8552d1
Martix <martin.quivron@icloud.com>
2022-03-04 22:30:36 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import discord from "discord.js";
 import { Client } from "./structure/Client"
 
-
-export const client = new Client('ODMwNDU4MjY1NDMwMTMwNzM5.YHG-hw.ukjMrTTm_uYmlmPAGIm80bX9QfM')
+/*
+export const client = new Client('token')
 client.init('./build/commands', './build/events')
-
+*/
